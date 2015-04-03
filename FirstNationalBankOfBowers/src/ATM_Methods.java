5d6b35cc5a0d458ec0fec9cfc495fcdcc78c9c91
Alexander <Alexander@ThrashKick->
2015-04-03 17:11:11 -0400

    Basic Startup. Didn't get much done because I don't know how the image files are going to hang up. I need to figure out how I want to call the information. I was thinking using an SSN as a simulation instead of a card swipe, with a little button just for testing.

---
+++ FirstNationalBankOfBowers/src/ATM_Methods.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alexander
+ */
+public class ATM_Methods {
+    
+    String UserInfoFname;
+    double UserAccountBal;
+    //Void to get user information and to String for output (Welcome User!)
+    public void setUserInfoFname(String setUserFname)
+    {
+        UserInfoFname = ("Welcome Back " + setUserFname + "!!");
+    }
+    //return variable for call
+    public String getUserInfoFname()
+    {
+        return UserInfoFname;
+    }
+    //set Account Balance
+    public void setAccountBalance(double setBalance)
+    {
+       UserAccountBal =  setBalance;
+               
+    }
+    //return variable
+    public double getAccountBalance()
+    {
+        return UserAccountBal;
+    }
+ 
+    
+}
